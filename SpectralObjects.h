bd378df7c5cdd08eb6df66293ba440472d4b9511
TommyMcPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-04-13 04:44:15 -0700

    Add files via upload

---
+++ SpectralObjects.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+#include <fstream>
+#include <cmath>
+
+void writeToFile(std::ofstream& file, int value, int size);
+float getSample();
+float noise();
+void render();
+float length;
+int sampleCount;
+std::ofstream wavFile;
+int preAudioP;
+int sampleInt;
+int postAudioP;
+const int sampleRate = 48000;
+const int byteDepth = 2;
+const int channels = 4;
+const int harmonics = 4800;
+float sample;
+float samples;
