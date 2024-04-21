a47bf040a63b0b12f3857914e82e7149797815e0
TommyMcPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-04-21 03:37:07 -0700

    Add files via upload

--- SpectralObjects.cpp
+++ SpectralObjects.cpp
@@ -5,28 +5,42 @@ void writeToFile(std::ofstream& file, int value, int size) {
 }
 
 void getSample() {
-    //sampleCount++;
-    monoSample = 0.0;
+    sampleCount++;
+    remainingProgress = 1.0 - (sampleCount / samples);
+    lfoPhaseAIncrement = pow(remainingProgress, 2.0) * fundamentalIncrement;
+    lfoPhaseBIncrement = pow(remainingProgress, 0.5) * fundamentalIncrement;
+    lfoPhaseA += lfoPhaseAIncrement;
+    lfoPhaseB += lfoPhaseBIncrement;
+    lfoPhaseA = fmod(lfoPhaseA, (float)wavetableSize);
+    lfoPhaseB = fmod(lfoPhaseB, (float)wavetableSize);
     sample = { 0.0, 0.0, 0.0, 0.0 };
     totalAmplitude = 0.0;
     for (int a = 0; a < harmonics; a++) {
         totalAmplitude += amplitudes[a];
     }
     for (int a = 0; a < harmonics; a++) {
-        phases[a] += noise() * jitterDepths[a] * phaseIncrements[a] + phaseIncrements[a];
-        phases[a] = fmod(phases[a], (float)wavetableSize);
-        partialSample = wavetable.at((int)(phases[a])) * amplitudes[a] / totalAmplitude;
-        monoSample += partialSample;
-        sample[0] += partialSample * sqrt(sqrt(1.0 - positions[a][0]) * sqrt(1.0 - positions[a][1]));
-        sample[1] += partialSample * sqrt(sqrt(positions[a][0]) * sqrt(1.0 - positions[a][1]));
-        sample[2] += partialSample * sqrt(sqrt(1.0 - positions[a][0]) * sqrt(positions[a][1]));
-        sample[3] += partialSample * sqrt(sqrt(positions[a][0]) * sqrt(positions[a][1]));
+        jitterPan = pan(jitterPositions[a], jitterDepths[a]);
+        partialPan = pan(positions[a], amplitudes[a]);
+        for (int b = 0; b < 4; b++) {
+            phases[a][b] += phaseIncrements[a] * noise() * jitterDepths[a] * jitterPan[b] + phaseIncrements[a];
+            phases[a][b] = fmod(phases[a][b], (float)wavetableSize);
+            partialSample[b] = wavetable.at((int)(phases[a][b])) / totalAmplitude;
+            sample[b] += partialSample[b] * partialPan[b];
+        }
     }
     //std::cout << sample << std::endl;
 }
 
 float noise() {
     return ((float)(rand() + 1.0) * 2 / (RAND_MAX + 1.0)) - 1;
+}
+
+std::array<float, 4> pan(std::array<float, 2> xy, float magnitude) {
+    std::array<float, 4> channelMagnitudes = { magnitude, magnitude, magnitude, magnitude };
+    channelMagnitudes[0] *= sqrt(sqrt(1.0 - xy[0]) * sqrt(1.0 - xy[1]));
+    channelMagnitudes[1] *= sqrt(sqrt(xy[0]) * sqrt(1.0 - xy[1]));
+    channelMagnitudes[2] *= sqrt(sqrt(1.0 - xy[0]) * sqrt(xy[1]));
+    channelMagnitudes[3] *= sqrt(sqrt(xy[0]) * sqrt(xy[1]));
 }
 
 void render() {
@@ -67,7 +81,7 @@ void render() {
 
 int main()
 {
-    fundamentalIncrement = (float)wavetableSize / (float)(harmonics * 2);
+    fundamentalIncrement = (float)wavetableSize / ((float)(harmonics * 2) * cutoff);
     for (int a = 0; a < wavetableSize; a++) {
         wavetable[a] = sin((float)a * 2.0 * M_PI / wavetableSize);
     }
