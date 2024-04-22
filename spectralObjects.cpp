0b6310d69c5fc43699b4414288e878004666c95a
TommyMcPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-04-22 06:09:17 -0700

    Add files via upload

--- spectralObjects.cpp
+++ spectralObjects.cpp
@@ -4,48 +4,99 @@ void writeToFile(std::ofstream& file, int value, int size) {
     file.write(reinterpret_cast<const char*> (&value), size);
 }
 
-float getSample() {
+void getSample() {
     sampleCount++;
-    sample = 0;
+    repositionInterval = pow(harmonics, (int)(2.0 * abs(progress - 0.5))) + 1;
+    if ((int)sampleCount % repositionInterval == 0) {
+        reposition = true;
+    }
+    else {
+        reposition = false;
+    }
+    progress = sampleCount / samples;
+    remainingProgress = 1.0 - progress;
+    lfoPhaseAIncrement = pow(remainingProgress, 2.0) * fundamentalIncrement;
+    lfoPhaseBIncrement = pow(remainingProgress, 0.5) * fundamentalIncrement;
+    lfoPhaseA += lfoPhaseAIncrement;
+    lfoPhaseB += lfoPhaseBIncrement;
+    lfoPhaseA = fmod(lfoPhaseA, (float)wavetableSize);
+    lfoPhaseB = fmod(lfoPhaseB, (float)wavetableSize);
+    lfoA = wavetable.at((int)lfoPhaseA);
+    lfoB = wavetable.at((int)lfoPhaseB);
+    totalAmplitude = 0.0;
     for (int a = 0; a < harmonics; a++) {
-        phases[a] += phaseIncrements[a];
-        phases[a] = fmod(phases[a], (float)wavetableSize);
-        partialSamples[a] = wavetable.at((int)(phases[a]));
-        sample += partialSamples[a] / (float)harmonics;
+        float partial = (float)(a + 1);
+        phaseIncrements[a] = ((float)a * pow(progress, 2.0) * 4.0 + 1.0) * fundamentalIncrement;
+        amplitudes[a] = (1.0 / partial) * fm(partial, lfoA);
+        totalAmplitude += amplitudes[a];
+        jitterDepths[a] = fm(partial, lfoB);
+        if (reposition) {
+            for (int b = 0; b < 2; b++) {
+                float lastPosition = positions[a][b];
+                float lastJitterPosition = jitterPositions[a][b];
+                positions[a][b] = move(lastPosition, lfoA);
+            }
+        }
+    }
+    sample = { 0.0, 0.0, 0.0, 0.0 };
+    for (int a = 0; a < harmonics; a++) {
+        jitterPan = pan(jitterPositions[a], jitterDepths[a]);
+        partialPan = pan(positions[a], amplitudes[a]);
+        for (int b = 0; b < 4; b++) {
+            phases[a][b] += phaseIncrements[a] * noise() * jitterDepths[a] * jitterPan[b] + phaseIncrements[a];
+            phases[a][b] = fmod(phases[a][b], (float)wavetableSize);
+            partialSample[b] = wavetable.at((int)(phases[a][b])) / totalAmplitude;
+            sample[b] += partialSample[b] * partialPan[b];
+        }
     }
     //std::cout << sample << std::endl;
-    return sample;
+}
+
+float fm(float partial, float lfo) {
+    return sin(partial * pow(lfo, 3.0) * M_PI * 2.0) * 0.5 + 0.5;
 }
 
 float noise() {
     return ((float)(rand() + 1.0) * 2 / (RAND_MAX + 1.0)) - 1;
 }
 
+float move(float last, float lfo) {
+    return 1.0 - abs(last - 0.5) * (noise() * 0.5 * lfo);
+}
+
+std::array<float, 4> pan(std::array<float, 2> xy, float magnitude) {
+    std::array<float, 4> channelMagnitudes = { magnitude, magnitude, magnitude, magnitude };
+    channelMagnitudes[0] *= sqrt(sqrt(1.0 - xy[0]) * sqrt(1.0 - xy[1]));
+    channelMagnitudes[1] *= sqrt(sqrt(xy[0]) * sqrt(1.0 - xy[1]));
+    channelMagnitudes[2] *= sqrt(sqrt(1.0 - xy[0]) * sqrt(xy[1]));
+    channelMagnitudes[3] *= sqrt(sqrt(xy[0]) * sqrt(xy[1]));
+    return channelMagnitudes;
+}
+
 void render() {
-    samples = length * (float)channels * (float)sampleRate;
+    samples = length * (float)sampleRate;
     wavFile.open("Test.wav", std::ios::binary);
-    // Header chunk
     wavFile << "RIFF";
     wavFile << "----";
     wavFile << "WAVE";
-    // Format chunk
     wavFile << "fmt ";
-    writeToFile(wavFile, byteDepth * 8, 4); // Size
-    writeToFile(wavFile, 1, 2); // Compression Code (PCM)
-    writeToFile(wavFile, channels, 2); // Channels
-    writeToFile(wavFile, sampleRate, 4); // Sample Rate
-    writeToFile(wavFile, sampleRate * byteDepth, 4); // Byte Rate
-    writeToFile(wavFile, byteDepth, 2); // Block Align
-    writeToFile(wavFile, byteDepth * 8, 2); // Bit Depth
-    // Data chunk
+    writeToFile(wavFile, byteDepth * 8, 4);
+    writeToFile(wavFile, 1, 2);
+    writeToFile(wavFile, channels, 2);
+    writeToFile(wavFile, sampleRate, 4);
+    writeToFile(wavFile, sampleRate * byteDepth, 4);
+    writeToFile(wavFile, byteDepth, 2);
+    writeToFile(wavFile, byteDepth * 8, 2);
     wavFile << "data";
     wavFile << "----";
     preAudioP = wavFile.tellp();
     auto maxSampleInt = pow(2, byteDepth * 8 - 1) - 1;
-    for (int a = 0; a < samples; a++) {
-        sample = getSample();
-        sampleInt = static_cast<int>(sample * maxSampleInt);
-        wavFile.write(reinterpret_cast<char*> (&sampleInt), byteDepth);
+    for (int c = 0; c < samples; c++) {
+        getSample();
+        for (int a = 0; a < 4; a++) {
+            sampleInt = static_cast<int>(sample[a] * maxSampleInt);
+            wavFile.write(reinterpret_cast<char*> (&sampleInt), byteDepth);
+        }
     }
     postAudioP = wavFile.tellp();
     wavFile.seekp(preAudioP - 4);
@@ -57,15 +108,15 @@ void render() {
 
 int main()
 {
-    wavetableSize = sqrt(2.0) * (float)pow(2, byteDepth * 8);
-    fundamentalIncrement = (float)wavetableSize / (float)(harmonics * 2);
+    fundamentalIncrement = (float)wavetableSize / ((float)(harmonics * 2) * cutoff * 4.0);
     for (int a = 0; a < wavetableSize; a++) {
-        wavetable.push_back(sin((float)a * 2.0 * M_PI / wavetableSize));
+        wavetable[a] = sin((float)a * 2.0 * M_PI / wavetableSize);
     }
     for (int a = 0; a < harmonics; a++) {
-        phaseIncrements[a] = (float)(a + 1) * fundamentalIncrement;
+        jitterDepths[a] = 0.5;
+        //length += (float)((a + 1) * harmonics) / (float)sampleRate;
     }
-    length = 3.0;
+    length = 60.0;
     sampleCount = 0;
     render();
 }
